e8d86a958a9bea71775b86ce98739550655d1311
Chayut66070504005 <chayut2548@gmail.com>
2026-02-05 17:48:04 +0700

    Update settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -98,6 +98,7 @@ DATABASES = {
          'PORT': '1433',
          'OPTIONS': {
              'driver': 'ODBC Driver 18 for SQL Server',
+             'extra_params': 'TrustServerCertificate=yes;',
          },
      },
  }
